74d979d71e0864ed15461a6f178c45945313c8d0
Oliver Luan <xiangming.luan@gmail.com>
2017-05-22 16:57:12 +0800

    redirect after POST, and show all items in template

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -11,6 +11,11 @@ class NewVistorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = self.browser.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# 用户查看应用首页
 		self.browser.get('http://localhost:8000')
@@ -31,14 +36,7 @@ class NewVistorTest(unittest.TestCase):
 		# 用户按回车键后页面更新了
 		# 待办事项表格中显示了‘1. Buy peacock feathers’
 		inputbox.send_keys(Keys.ENTER)
-
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = self.browser.find_elements_by_tag_name('tr')
-		# self.assertTrue(
-		# 	any(row.text == '1. Buy peacock feathers' for row in rows),
-		# 	'New to-do item did not appear in table --its text was:\n%s' %(table.text,)
-			# )
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		# 页面中又显示了一个输入框，可以输入其他待办事项
 		# 用户输入了‘Use peacock feathers to make a fly’
@@ -48,10 +46,8 @@ class NewVistorTest(unittest.TestCase):
 		inputbox.send_keys(Keys.ENTER)
 
 		# 页面再次更新， 清单中显示了这两个待办事项
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = self.browser.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [rows[0].text for row in rows])
-		self.assertIn('2: Use peacock feathers to make a fly', [rows[1].text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 		# 用户想知道这个网站是否会记住他的清单
 		self.fail('Finish the test!')
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,3 +1,4 @@
+from lists.models import Item
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
@@ -21,11 +22,59 @@ class HomePageTest(TestCase):
 		# 由于模版添加了csrf_token， 这个断言不会成功
 		# self.assertEqual(response.content.decode(), expected_html)
 		
-	def tets_home_page_can_save_a_POST_requesy(self):
+	def test_home_page_can_save_a_POST_request(self):
 		request  = HttpRequest()
 		request.method = 'POST'
 		request.POST['item_text'] = 'A new list item'
 
 		response = home_page(request)
-		self.assertIn('A new list item', response.content.decode())
+
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
+
+	def test_home_page_only_saves_items_when_necessary(self):
+		request = HttpRequest()
+		home_page(request)
+		self.assertEqual(Item.objects.count(), 0)
+
+	def test_home_page_redirects_after_POST(self):
+		request  = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+
+		response = home_page(request)
+
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response['location'], '/')
+
+	def test_home_page_display_all_list_items(self):
+		Item.objects.create(text='itemey 1')
+		Item.objects.create(text='itemey 2')
+
+		request = HttpRequest()
+		response = home_page(request)
+
+		self.assertIn('itemey 1', response.content.decode())
+		self.assertIn('itemey 2', response.content.decode())
+
+
+class ItemModelTest(TestCase):
+	"""docstring for ItemModelTest"""
+	def test_saving_and_retrieving_items(self):
+		first_item = Item()
+		first_item.text = 'The first (ever) list item'
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'Item the second'
+		second_item.save()
+
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(), 2)
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(second_saved_item.text, 'Item the second')
+		
 
