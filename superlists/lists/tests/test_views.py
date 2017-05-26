e715312a360815093a630d36e3d9505ef36d2f4f
Oliver Luan <xiangming.luan@gmail.com>
2017-05-26 11:15:45 +0800

    1. handle and test POST error in view_list 2. refactor urls in templates with names in url mappings

--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -74,6 +74,14 @@ class ListViewTest(TestCase):
 			)
 		self.assertRedirects(response, '/lists/%d/' %(correct_list.id))
 
+	def test_validation_errors_end_up_on_lists_page(self):
+		list_ = List.objects.create()
+		response = self.client.post('/lists/%d/' %(list_.id),
+			data={'item_text':''})
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'list.html')
+		expected_error = 'You cannot have an empty list item'
+		self.assertContains(response, expected_error)
 
 class NewListTest(TestCase):
 	"""docstring for NewListTest"""
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -9,10 +9,18 @@ def home_page(request):
 
 def view_list(request, list_id):
 	list_ = List.objects.get(id=list_id)
+	error = None
+
 	if request.method == 'POST':
-		Item.objects.create(text=request.POST['item_text'], list=list_)
-		return redirect('/lists/%d/' %(list_.id))
-	return render(request, 'list.html', {'list': list_})
+		try:
+			item = Item(text=request.POST['item_text'], list=list_)
+			item.full_clean()
+			item.save()
+			return redirect('/lists/%d/' %(list_.id))
+		except ValidationError:
+			error = 'You cannot have an empty list item'
+			
+	return render(request, 'list.html', {'list': list_, 'error': error})
 
 def new_list(request):
 	list_ = List.objects.create()
